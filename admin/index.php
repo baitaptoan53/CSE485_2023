1aab914a2f8c2dd4bfd13055e011c1d0105b6e91
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2023-02-22 14:50:27 +0700

    count user,theloai,,tac,gia

---
+++ admin/index.php
@@ -0,0 +1,164 @@
+<?php
+// ket noi database
+$type     = 'mysql';                 // Type of database
+$server   = 'localhost';             // Server the database is on
+$db       = 'btth01_cse485';             // Name of the database
+$port     = '3306';                      // Port is usually 8889 in MAMP and 3306 in XAMPP             // UTF-8 encoding using 4 bytes of data per character
+
+$username = 'root';          // Enter YOUR username here
+$password = '';         // Enter YOUR password here
+
+$options  = [                        // Options for how PDO works
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];                                                                  // Set PDO options
+
+// DO NOT CHANGE ANYTHING BENEATH THIS LINE
+$dsn = "$type:host=$server;dbname=$db;port=$port"; // Create DSN
+try {                                                               // Try following code
+    $pdo = new PDO($dsn, $username, $password, $options);           // Create PDO object                               // If successful, print message
+} catch (PDOException $e) {                                         // If exception thrown
+    echo $e->getMessage();                                          // Print error message
+}       // Re-throw exception
+$sql_count_user = "SELECT COUNT(*) FROM users";
+$sql_count_theloai = "SELECT COUNT(*) FROM theloai";
+$sql_count_tacgia = "SELECT COUNT(*) FROM tacgia";
+$sql_count_baihat = "SELECT COUNT(*) FROM baiviet";
+
+// thuc thi cau lenh
+$stmt_count_user = $pdo->prepare($sql_count_user);
+$stmt_count_theloai = $pdo->prepare($sql_count_theloai);
+$stmt_count_tacgia = $pdo->prepare($sql_count_tacgia);
+$stmt_count_baihat = $pdo->prepare($sql_count_baihat);
+count user,theloai,,tac,gia
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music for Life</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style_login.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
+            <div class="container-fluid">
+                <div class="h3">
+                    <a class="navbar-brand" href="#">Administration</a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active fw-bold" aria-current="page" href="./">Trang chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../index.php">Trang ngoài</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="category.php">Thể loại</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="author.php">Tác giả</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="article.php">Bài viết</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+    <main class="container mt-5 mb-5">
+        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="card mb-2" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">
+                            <a href="" class="text-decoration-none">Người dùng</a>
+                        </h5>
+                        <h5 class="h1 text-center">
+                            <?php
+                            $stmt_count_user->execute();
+                            $count_user = $stmt_count_user->fetchColumn();
+                            echo $count_user;
+                            ?>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card mb-2" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">
+                            <a href="" class="text-decoration-none">Thể loại</a>
+                        </h5>
+
+                        <h5 class="h1 text-center">
+                            <?php
+                            $stmt_count_theloai->execute();
+                            $count_theloai = $stmt_count_theloai->fetchColumn();
+                            echo $count_theloai;
+                            ?>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card mb-2" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">
+                            <a href="" class="text-decoration-none">Tác giả</a>
+                        </h5>
+
+                        <h5 class="h1 text-center">
+                            <?php
+                            $stmt_count_tacgia->execute();
+                            $count_tacgia = $stmt_count_tacgia->fetchColumn();
+                            echo $count_tacgia;
+                            ?>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card mb-2" style="width: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">
+                            <a href="" class="text-decoration-none">Bài viết</a>
+                        </h5>
+                        <h5 class="h1 text-center">
+                            <?php
+                            $stmt_count_baiviet->execute();
+                            $count_baiviet = $stmt_count_baiviet->fetchColumn();
+                            echo $count_baiviet;
+                            ?>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
+        <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
