ee036b7727d52a9a17d6c5cf8d29da809405b08a
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2023-02-22 17:35:11 +0700

    category page

--- admin/index.php
+++ admin/index.php
@@ -16,6 +16,7 @@ $sql_count_baihat = "SELECT COUNT(*) FROM baiviet";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -57,26 +58,6 @@ $sql_count_baihat = "SELECT COUNT(*) FROM baiviet";
                     </ul>
                     <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active fw-bold" aria-current="page" href="./">Trang chủ</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../index.php">Trang ngoài</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="category.php">Thể loại</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="author.php">Tác giả</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="article.php">Bài viết</a>
-                            </li>
-                        </ul>
-
-                    </div>
                 </div>
         </nav>
     </header>
